ae83ab170873c3d1beabb79b1f98eeebac251d91
Denver Timlick <denvertimlick@gmail.com>
2025-04-10 13:34:46 -0600

    entered the first variables for java

--- lab-6/scripts/script.js
+++ lab-6/scripts/script.js
@@ -10,9 +10,8 @@ You are encouraged to use the provided naming convention for ease of review.
 /* create variables to hold the values for modelName and duration */
 
 // INSERT YOUR CODE HERE
-
-
-
+let modelName = 1
+let duration = 1
 
 
 /****************** helper function ******************/
